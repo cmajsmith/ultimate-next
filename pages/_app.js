0238e9a5aecdcfd1d957fc10c222ee35cc2f2de6
cmajorsmith <cmajorsmith@gmail.com>
2022-01-06 16:35:05 +0200

    SEO support added

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.scss'
 import BaseLayout from "../components/layouts/base/BaseLayout";
+import {DefaultSeo} from "next-seo";
 
 function MyApp({ Component, pageProps }) {
   return (
       <BaseLayout>
+        <DefaultSeo
+            title="Default Page Title"
+            description="Default Page Description"
+        />
         <Component {...pageProps} />
       </BaseLayout>
   )
